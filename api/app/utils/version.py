114e744a4ee868eff44d5f9bd2aea3c13709b781
Scott Idler <scott.a.idler@gmail.com>
2016-11-22 13:47:32 -0800

    used global _version to cache result instead of hit disk everytime

--- api/app/utils/version.py
+++ api/app/utils/version.py
@@ -11,10 +11,14 @@ def describe():
     from subprocess import check_output
     return check_output('git describe', shell=True).decode('utf-8').strip()
 
+
+_version = None
 def version():
+    global _version
     versionfile = dirname(__file__, 3) + '/VERSION'
-    version = open(versionfile).read() if os.path.exists(versionfile) else describe()
-    version, *suffix = version.split('-')
-    if suffix:
-        version = version.replace('v', '') + '.dev{0}+{1}'.format(*suffix)
-    return version
+    if not _version:
+        _version = open(versionfile).read() if os.path.exists(versionfile) else describe()
+        _version, *suffix = _version.split('-')
+        if suffix:
+            _version = _version.replace('v', '') + '.dev{0}+{1}'.format(*suffix)
+    return _version
