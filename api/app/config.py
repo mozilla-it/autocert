50f2a2741e497fc7568890e04c0dfc147e7a9876
Scott Idler <scott.a.idler@gmail.com>
2016-12-12 14:33:51 -0800

    added functions to load, write and update the config

--- api/app/config.py
+++ api/app/config.py
@@ -10,13 +10,23 @@ from attrdict import AttrDict
 from urlpath import URL
 from pathlib2 import Path
 
-class ConfigFileError(Exception):
-    def __init__(pattern, errors=None):
-        message = 'Could not find config file with pattern=%s'.format(pattern)
-        super(ConfigFileError, self).__init__(message)
-        self.errors = errors
+try:
+    from app.utils.dictionary import merge
+except ImportError as ie:
+    from utils.dictionary import merge
+
+
+CONFIG_DIR = os.path.dirname(__file__)
+CONFIG_YML = '{0}/config.yml'.format(CONFIG_DIR)
+DOT_CONFIG_YML = '{0}/.config.yml'.format(CONFIG_DIR)
 
 class ConfigLoadError(Exception):
+    def __init__(config, errors=None):
+        message = 'Error loading config file =%s'.format(config)
+        super(ConfigLoadError, self).__init__(message)
+        self.errors = errors
+
+class ConfigWriteError(Exception):
     def __init__(config, errors=None):
         message = 'Error loading config file =%s'.format(config)
         super(ConfigLoadError, self).__init__(message)
@@ -33,13 +43,6 @@ class ApikeyNotFoundError(Exception):
         message = 'Could not find apikey in this dict %s' % d
         super(ApikeyNotFoundError, self).__init__(message)
         self.errors = errors
-
-def _divine_config(pattern='config.yml*'):
-    dirpath = os.path.dirname(__file__)
-    matches = glob('/'.join([dirpath, pattern]))
-    if matches:
-        return sorted(matches)[0]
-    raise ConfigFileError(pattern)
 
 def _fixup(obj):
     if isinstance(obj, dict):
@@ -59,14 +62,34 @@ def _fixup(obj):
         return d
     return obj
 
-def _load_config():
-    config = _divine_config()
-    try:
-        obj = yaml.safe_load(open(config))
-        obj = _fixup(obj)
-    except Exception as ex:
-        print('ex =', ex)
-        raise ConfigLoadError(config, errors=[ex])
-    return AttrDict(obj)
+def _load_config(filename, fixup=True):
+    cfg = {}
+    if os.path.isfile(filename):
+        try:
+            with open(filename, 'r') as f:
+                cfg = yaml.round_trip_load(f.read())
+            if fixup:
+                cfg = _fixup(cfg)
+        except Exception as ex:
+            print('ex =', ex)
+            raise ConfigLoadError(filename, errors=[ex])
+    return AttrDict(cfg)
 
-CFG = _load_config()
+def _write_config(filename, cfg):
+    try:
+        with open(filename, 'w') as f:
+            f.write(yaml.round_trip_dump(dict(cfg), indent=4))
+    except Exception as ex:
+        raise ConfigWriteError(filename, errors=[ex])
+    return cfg
+
+def _update_config(filename, *cfgs):
+    if cfgs:
+        updated = _load_config(filename, fixup=False)
+        for cfg in cfgs:
+            if not isinstance(cfg, AttrDict):
+                cfg = AttrDict(cfg)
+            updated += cfg
+    return _write_config(filename, updated)
+
+CFG = _load_config(DOT_CONFIG_YML)
