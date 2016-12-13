b4db374b15d36720caa3d306f2870781a0536555
Scott Idler <scott.a.idler@gmail.com>
2016-12-12 21:15:53 -0800

    added debug statements to routes

--- api/app/main.py
+++ api/app/main.py
@@ -77,25 +77,25 @@ def hello(target='world'):
     return jsonify({'msg': 'hello %(target)s' % locals()})
 
 def digicert_list_certs():
+    app.logger.info('digicert_list_certs called')
     response = requests.get(
         CFG.authorities.digicert.baseurl / 'order/certificate',
         auth=CFG.authorities.digicert.auth,
         headers=CFG.authorities.digicert.headers)
     if response.status_code == 200:
-        obj = response.json()
-        certs = [cert for cert in obj['orders'] if is_valid_cert(cert['status'])]
+        from pprint import pformat
+        obj = AttrDict(response.json())
+        app.logger.debug('digicert_list_certs: obj.orders="{0}"'.format(pformat(obj.orders)))
+        certs = [ cert for cert in obj.orders if is_valid_cert(cert.status) ]
+        app.logger.debug('digicert_list_certs: certs="{0}"'.format(pformat(certs)))
         return {
-            'certs': certs,
+            'certs': list(certs),
         }
-#        obj = AttrDict(response.json())
-#        certs = [ cert for cert in obj.orders if is_valid_cert(cert.status) ]
-#        return {
-#            'certs': list(certs),
-#        }
     else:
         app.logger.error('failed request to /list/certs with status_code={0}'.format(response.status_code))
 
 def letsencrypt_list_certs():
+    app.logger.info('letsencrypt_list_certs called')
     return {
         'certs': []
     }
@@ -110,10 +110,12 @@ AUTHORITIES = {
 def list_certs(pattern='*'):
     app.logger.info('/list/certs called with pattern="{pattern}"'.format(**locals()))
     authorities = [authority for authority in AUTHORITIES.keys() if fnmatch(authority, pattern)]
+    app.logger.debug('authorities="{authorities}"'.format(**locals()))
     funcs = [AUTHORITIES[authority] for authority in authorities]
     results = [func() for func in funcs]
+    app.logger.debug('len(results)={0}'.format(len(results)))
     for result in results:
-        app.logger.warning('certs.count = "{0}"'.format(len(result['certs'])))
+        app.logger.debug('1) len(result["certs"])="{0}"'.format(len(result['certs'])))
     result = merge(*results)
-    app.logger.warning('certs.count = "{0}"'.format(len(result['certs'])))
+    app.logger.debug('2) len(result["certs"])="{0}"'.format(len(result['certs'])))
     return jsonify(result)
