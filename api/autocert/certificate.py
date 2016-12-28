c77ae618836f8e9b0495bfe436e6955cf7907817
Scott Idler <scott.a.idler@gmail.com>
2016-12-28 11:43:05 -0800

    refactored creation and loading of key and csr files

--- api/autocert/certificate.py
+++ api/autocert/certificate.py
@@ -52,16 +52,15 @@ ENCODING = {
     'PEM': serialization.Encoding.PEM,
 }
 
-def create_key(common_name, **kwargs):
+def _create_key(common_name, **kwargs):
     app.logger.info('called create_key:\n{0}'.format(pformat(locals())))
-    keyfile = CFG.key.dirpath / '{common_name}.key'.format(**locals())
     key = rsa.generate_private_key(
         public_exponent=kwargs.get('public_exponent', CFG.key.public_exponent),
         key_size=kwargs.get('key_size', CFG.key.key_size),
         backend=default_backend())
     return key
 
-def create_oids(common_name, oids):
+def _create_oids(common_name, oids):
     attrs = [x509.NameAttribute(NameOID.COMMON_NAME, common_name)]
     for key in CFG.csr.oids:
         oid = OIDS_MAP.get(key, None)
@@ -73,45 +72,51 @@ def create_oids(common_name, oids):
             attrs += [x509.NameAttribute(oid, value)]
     return attrs
 
-def add_sans(subject, sans):
+def _add_sans(subject, sans):
     subject.add_extension(
         [x509.DNSName(dns_name) for dns_name in dns_names],
         critical=False)
 
-def create_csr(key, common_name, oids=None, sans=None):
+def _create_csr(common_name, key, oids=None, sans=None):
     app.logger.info('called create_csr:\n{0}'.format(pformat(locals())))
-    csrfile = CFG.key.dirpath / '{common_name}.csr'.format(**locals())
     builder = x509.CertificateSigningRequestBuilder()
-    oids = create_oids(common_name, oids if oids else {})
+    oids = _create_oids(common_name, oids if oids else {})
     subject = builder.subject_name(x509.Name(oids))
     if sans:
-        add_sans(subject, sans)
+        _add_sans(subject, sans)
     csr = subject.sign(key, hashes.SHA256(), default_backend())
     return csr
 
-def key_info_and_bytes(common_name, key):
-    key_bytes = BytesIO(key.private_bytes(
+def create_key_and_csr(common_name, oids=None, sans=None):
+    key = _create_key(common_name)
+    csr = _create_csr(common_name, key)
+    key_text = key.private_bytes(
         encoding=ENCODING[CFG.key.encoding],
         format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption()))
-    key_info = tarfile.TarInfo(common_name+'.key')
-    key_info.size = key_bytes.getbuffer().nbytes
-    return key_info, key_bytes
+        encryption_algorithm=serialization.NoEncryption()).decode('utf-8')
+    csr_text = csr.public_bytes(ENCODING[CFG.csr.encoding]).decode('utf-8')
+    return key_text, csr_text
 
-def csr_info_and_bytes(common_name, csr):
-    csr_bytes = BytesIO(csr.public_bytes(ENCODING[CFG.csr.encoding]))
-    csr_info = tarfile.TarInfo(common_name+'.csr')
-    csr_info.size = csr_bytes.getbuffer().nbytes
-    return csr_info, csr_bytes
+def key_tarinfo(common_name, key):
+    info = tarfile.TarInfo(common_name+'.key')
+    info.size = len(key)
+    return info
 
-def create_tar(common_name, suffix, key, csr, metadata=None):
-    filename = CFG.tar.dirpath / '{common_name}.{suffix}.tar.gz'.format(**locals())
-    with tarfile.open(str(filename), 'w:gz') as tar:
-        key_info, key_bytes = key_info_and_bytes(common_name, key)
-        tar.addfile(key_info, fileobj=key_bytes)
-        csr_info, csr_bytes = csr_info_and_bytes(common_name, csr)
-        tar.addfile(csr_info, csr_bytes)
+def csr_tarinfo(common_name, csr):
+    info = tarfile.TarInfo(common_name+'.csr')
+    info.size = len(csr)
+    return info
+
+def tar_key_and_csr(common_name, suffix, key, csr, metadata=None):
+    filename = str(CFG.tar.dirpath / '{common_name}.{suffix}.tar.gz'.format(**locals()))
+    with tarfile.open(filename, 'w:gz') as tar:
+        tar.addfile(key_tarinfo(common_name, key), BytesIO(key.encode('utf-8')))
+        tar.addfile(csr_tarinfo(common_name, csr), BytesIO(csr.encode('utf-8')))
     return filename
 
-def load_key_and_csr(filename):
-    pass
+def untar_key_and_csr(common_name, suffix):
+    filename = str(CFG.tar.dirpath / '{common_name}.{suffix}.tar.gz'.format(**locals()))
+    with tarfile.open(filename, 'r:gz') as tar:
+        key = tar.extractfile('{common_name}.key'.format(**locals())).read().decode('utf-8')
+        csr = tar.extractfile('{common_name}.csr'.format(**locals())).read().decode('utf-8')
+    return key, csr
