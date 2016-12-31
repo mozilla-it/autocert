2f4512649022af40254141e83ee2091cfafb08ea
Scott Idler <scott.a.idler@gmail.com>
2016-12-31 09:57:43 -0800

    added get installed certs function to zeus

--- api/autocert/zeus.py
+++ api/autocert/zeus.py
@@ -6,7 +6,11 @@ module specific to talking with zeus load balancer
 
 import requests
 from attrdict import AttrDict
-from pprint import pformat
+from pprint import pprint, pformat
+from fnmatch import fnmatch
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 from autocert.utils.dictionary import merge
 
@@ -20,21 +24,37 @@ try:
 except ImportError:
     from config import CFG
 
-def request(method, path, json=None):
-    authority = CFG.destinations.zeus
+def request(method, zeusdest, path, json=None):
+    authority = AttrDict(CFG.destinations.zeus[zeusdest])
     url = authority.baseurl / path
-    response = requests.request(method, url, auth=authority.auth, json=json)
+    response = requests.request(method, url, verify=False, auth=authority.auth, headers=authority.headers, json=json)
     try:
-        ad = AttrDict(response.json())
+        obj = AttrDict(response.json())
     except ValueError:
-        ad = None
-    return response, ad
+        obj = None
+    return response, obj
 
-def get(path):
-    return request('GET', path)
+def get(zeusdest, path):
+    return request('GET', zeusdest, path)
 
-def put(path, json=None):
-    return request('PUT', path, json=json)
+def put(zeusdest, path, json=None):
+    return request('PUT', zeusdest, path, json=json)
 
-def post(path, json=None):
-    return request('POST', path, json=json)
+def post(zeusdest, path, json=None):
+    return request('POST', zeusdest, path, json=json)
+
+def get_installed_certificates(zeusdest, pattern='*'):
+    r, o = get(zeusdest, 'ssl/server_keys')
+    certs = {}
+    if r.status_code == 200:
+        for record in o.children:
+            name = record['name']
+            if fnmatch(name, pattern):
+                r, o = get(zeusdest, 'ssl/server_keys/' + name)
+                if r.status_code == 200:
+                    certs.update({name : dict(o.properties.basic)})
+                else:
+                    print(r.status_code, r.text)
+    else:
+        print(r.status_code, r.text)
+    return {zeusdest: certs}
