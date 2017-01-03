e98b53b2842ac1640678f624e5ed94e2db4170e7
Scott Idler <scott.a.idler@gmail.com>
2017-01-03 12:43:14 -0800

    added verbose_parse to be used in all subcommands

---
+++ cli/verbose.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+cli.verbose
+'''
+
+from argparse import ArgumentParser
+
+verbose_parser = ArgumentParser(add_help=False)
+verbose_parser.add_argument(
+    '--verbose',
+    metavar='LEVEL',
+    default=0,
+    const=1,
+    type=int,
+    nargs='?',
+    help='set verbosity level')
+
