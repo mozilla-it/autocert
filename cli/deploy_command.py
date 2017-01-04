1a5ba99dc33d1307cd2c0603c2054ef07c0b1573
Scott Idler <scott.a.idler@gmail.com>
2017-01-04 15:52:10 -0800

    added deploy command

---
+++ cli/deploy_command.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+cli.deploy
+'''
+
+import requests
+
+from cli.utils.output import output
+from cli.verbose import verbose_parser
+
+DESTINATIONS = [
+    'zeus:scl3-ext',
+    'zeus:scl3-int',
+    'zeus:phx1-ext',
+    'zeus:phx1-int',
+    'zeus:test',
+]
+
+def add_parser(subparsers):
+    parser = subparsers.add_parser('deploy', parents=[verbose_parser])
+    parser.add_argument(
+        'common_name',
+        help='common name')
+    parser.add_argument(
+        '-d', '--destinations',
+        metavar='DEST',
+        required=True,
+        choices=DESTINATIONS,
+        nargs='+',
+        help='default="%(default)s"; choose which destinations for install')
+    parser.set_defaults(func=do_deploy)
+
+def dictify(destinations, sep=':'):
+    result = {}
+    for destination in destinations:
+        key, value = destination.split(sep)
+        result[key] = result.get(key, []) + [value]
+    return result
+
+def do_deploy(ns):
+    json = {
+        'common_name': ns.common_name,
+        'destinations': dictify(ns.destinations),
+    }
+    response = requests.put(ns.api_url / 'auto-cert', json=json)
+    if response.status_code == 201:
+        certs = response.json()['certs']
+        if not ns.verbose:
+            pass
+        output(certs)
+        return
+    else:
+        print(response)
+        print(response.text)
+    raise Exception('wtf do_deploy')
+
