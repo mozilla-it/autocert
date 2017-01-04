6cd702e795f02f7bc6a79621491599afa8e0878f
Scott Idler <scott.a.idler@gmail.com>
2017-01-04 13:50:45 -0800

    moved output to utils and added str repr to print multiline scalars as literal blocks

---
+++ api/autocert/utils/output.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+output
+'''
+
+try:
+    from ruamel import yaml
+    from urlpath import URL
+    from attrdict import AttrDict
+    from packaging.version import parse as version_parse
+except ImportError as ie:
+    print(ie)
+    print('perhaps you need to install cli/requirements.txt via pip3')
+
+
+def str_presenter(dumper, data):
+    str_tag = 'tag:yaml.org,2002:str'
+    if len(data.splitlines()) > 1:
+        return dumper.represent_scalar(str_tag, data, style='|')
+    return dumper.represent_scalar(str_tag, data)
+
+yaml.add_representer(str, str_presenter)
+
+def yaml_format(obj):
+    class MyDumper(yaml.Dumper):
+        def represent_mapping(self, tag, mapping, flow_style=False):
+            return yaml.Dumper.represent_mapping(self, tag, mapping, flow_style)
+    return yaml.dump(obj, Dumper=MyDumper).strip()
+
+def output(obj):
+    return print(yaml_format(obj))
+
