311050b9ddb966c0eb5994ee0a1ce85bde0bc027
Scott Idler <scott.a.idler@gmail.com>
2017-01-23 23:38:55 -0800

    added more magic to fmt and pfmt

--- api/utils/format.py
+++ api/utils/format.py
@@ -5,7 +5,7 @@ format: here there be baby dragons!
 '''
 
 import inspect
-from pprint import pformat
+from pprint import pprint, pformat
 
 from utils.dictionary import merge
 
@@ -14,15 +14,30 @@ def fmt_dict(obj):
         return pformat(obj)
     return str(obj)
 
-def fmt(string, *args, **kwargs):
+def _fmt(string, args, kwargs, do_print=False):
     '''
     here there be baby dragons!
     '''
-    if args or kwargs:
-        return string.format(
-            *[fmt_dict(arg) for arg in args],
-            **{k:fmt_dict(v) for k,v in kwargs.items()})
-    frame = inspect.currentframe().f_back
-    gl = frame.f_globals
-    gl.update(frame.f_locals)
-    return string.format(**{k:fmt_dict(v) for k,v in gl.items()})
+    try:
+        if args or kwargs:
+            return string.format(
+                *[fmt_dict(arg) for arg in args],
+                **{k:fmt_dict(v) for k,v in kwargs.items()})
+        frame = inspect.currentframe().f_back.f_back
+        gl = dict(locals=frame.f_locals, globals=frame.f_globals)
+        gl.update(frame.f_globals)
+        gl.update(frame.f_locals)
+        s = string.format(**{k:fmt_dict(v) for k,v in gl.items()})
+    except KeyError as ke:
+        print('keyerror not found in following keys of gl dict:')
+        pprint(list(gl.keys()))
+        raise ke
+    if do_print:
+        print(s)
+    return s
+
+def fmt(string, *args, **kwargs):
+    return _fmt(string, args, kwargs)
+
+def pfmt(string, *args, **kwargs):
+    return _fmt(string, args, kwargs, do_print=True)
