02729690732b70de7f57a314339bc27618ab24e9
Scott Idler <scott.a.idler@gmail.com>
2017-02-07 10:06:02 -0800

    changed to add in authority and destination details at verbosity 1 and beyond

--- api/endpoint/display.py
+++ api/endpoint/display.py
@@ -4,7 +4,6 @@
 import os
 import re
 
-from pprint import pformat, pprint
 from attrdict import AttrDict
 from fnmatch import fnmatch
 from ruamel import yaml
@@ -12,16 +11,14 @@ from ruamel import yaml
 from autocert import digicert
 from autocert import zeus
 from utils import tar
-
 from utils.dictionary import merge, head, head_body
-
+from utils.format import fmt, pfmt
 from app import app
-
 from config import CFG
-
 from endpoint.base import EndpointBase
 from authority.factory import create_authority
 from destination.factory import create_destination
+from utils.output import yaml_format
 
 class DisplayEndpoint(EndpointBase):
     def __init__(self, cfg, verbosity):
@@ -31,7 +28,7 @@ class DisplayEndpoint(EndpointBase):
         status = 200
         cert_name_pns = [self.sanitize(cert_name_pn) for cert_name_pn in self.args.cert_name_pns]
         certs = self.tardata.get_certdata_from_tarfiles(*cert_name_pns)
-        if self.verbosity > 3:
+        if self.verbosity > 0:
             certs = self.authorities.digicert.display_certificates(certs)
             certs = self.destinations.zeus.fetch_certificates(certs, 'test2')
         json = self.transform(certs)
