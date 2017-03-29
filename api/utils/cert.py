89ae0ab111247e2d50b77a31cc5b9b61ca2b5eb4
Scott Idler <scott.a.idler@gmail.com>
2017-03-28 17:25:25 -0700

    added to_disk and from_disk impl with tests

--- api/utils/cert.py
+++ api/utils/cert.py
@@ -6,6 +6,8 @@ utils.cert
 
 import copy
 
+from pprint import pprint
+
 from utils import tar
 from utils.format import fmt, pfmt
 from utils.isinstance import *
@@ -26,9 +28,31 @@ class Cert(object):
         self.authority      = authority
         self.destinations   = destinations
 
+    def __repr__(self):
+        return yaml_format(self.to_json())
+
+    def __eq__(self, cert):
+        return (
+            self.common_name == cert.common_name and
+            self.timestamp == cert.timestamp and
+            self.key == cert.key and
+            self.csr == cert.csr and
+            self.crt == cert.crt and
+            self.expiry == cert.epiry and
+            self.authority == cert.authority) #and
+            #self.destinations == cert.destinations)
+
     @staticmethod
-    def from_disk(tarpath):
-        pass
+    def from_disk(tarpath, cert_name):
+        key, csr, crt, yml = tar.unbundle(tarpath, cert_name)
+        return Cert(
+            yml.pop('common_name', None),
+            yml.pop('timestamp', None),
+            key,
+            csr,
+            crt,
+            expiry=yml.pop('expiry', None),
+            authority=yml)
 
     @staticmethod
     def from_json(json):
@@ -51,14 +75,18 @@ class Cert(object):
                 files[self.cert_name + ext] = content
         return files
 
-    def __repr__(self):
-        return yaml_format(self.to_json())
-
     def to_disk(self, tarpath):
+        print('to_disk:')
         yml = copy.deepcopy(self.authority)
+        print('yml')
+        pprint(yml)
         yml.pop('key', None)
         yml.pop('csr', None)
         yml.pop('crt', None)
+        yml.update(dict(
+            common_name=self.common_name,
+            timestamp=self.timestamp,
+            expiry=self.expiry))
         return tar.bundle(
             tarpath,
             self.cert_name,
--- tests/test_cert.py
+++ tests/test_cert.py
@@ -6,6 +6,7 @@ import pytest
 
 from subprocess import check_output
 
+from utils.format import fmt, pfmt
 from utils.cert import Cert
 
 DIR = os.path.dirname(os.path.realpath(__file__))
@@ -41,5 +42,17 @@ def test_files(cert):
 def test_to_disk(cert, tmpdir):
     tarpath = tmpdir.mkdir('tarpath')
     tarfile = cert.to_disk(tarpath)
-    print('\ntarfile =', tarfile)
-    print(check_output('tar -tvf ' + tarfile))
+    output = check_output('tar -tvf ' + tarfile, shell=True).decode('utf-8')
+    assert cert.cert_name + '.key' in output
+
+def test_roundtrip(cert, tmpdir, capsys):
+    tarpath = tmpdir.mkdir('tarpath')
+    tarfile = cert.to_disk(tarpath)
+    cert2 = Cert.from_disk(tarpath, cert.cert_name)
+    assert cert.common_name == cert2.common_name
+    assert cert.timestamp == cert2.timestamp
+    assert cert.key == cert2.key
+    assert cert.csr == cert2.csr
+    assert cert.crt == cert2.crt
+    assert cert.expiry == cert2.expiry
+    assert cert.authority == cert2.authority
