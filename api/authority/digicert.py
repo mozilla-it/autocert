d8334e22f22414ec4e9402d623b5ea8671d847c1
Scott Idler <scott.a.idler@gmail.com>
2018-04-02 16:54:52 -0700

    added some debug

--- api/authority/digicert.py
+++ api/authority/digicert.py
@@ -352,10 +352,11 @@ class DigicertAuthority(AuthorityBase):
             repeat_delta = timedelta(seconds=repeat_delta)
         path='order/certificate?filters[status]=reissue_processing'
         def reissue_processing(call):
+            app.logger.debug(fmt('call.recv.status = {0}', call.recv.status))
             processing_ids = [order.id for order in call.recv.json.orders]
             is_reissue_processing = any([True for order_id in order_ids if order_id in processing_ids])
             app.logger.debug(fmt('FIXME: are any of order_ids={order_ids} in processing_ids={processing_ids} => {is_reissue_processing}'))
-            return is_reissue_processing
+            return call.recv.status != 200 or is_reissue_processing
         call = self.get(path=path, repeat_delta=repeat_delta, repeat_if=reissue_processing)
         if call.recv.status != 200:
             raise EnsureOrderNotProcessingError(order_ids)
