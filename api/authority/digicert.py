6f84e86fdb598e814681cde205f9c007de2f29c8
Scott Idler <scott.a.idler@gmail.com>
2018-04-03 08:05:25 -0700

    fixed 1->2

--- api/authority/digicert.py
+++ api/authority/digicert.py
@@ -358,7 +358,7 @@ class DigicertAuthority(AuthorityBase):
                 is_reissue_processing = any([True for order_id in order_ids if order_id in processing_ids])
                 app.logger.debug(fmt('FIXME: are any of order_ids={order_ids} in processing_ids={processing_ids} => {is_reissue_processing}'))
                 return is_reissue_processing
-            app.logger.debug(fmt('FIXME: status={0} url={1}\n text={1}', call.recv.status, call.send.url, call.recv.text))
+            app.logger.debug(fmt('FIXME: status={0} url={1}\n text={2}', call.recv.status, call.send.url, call.recv.text))
             return True
         call = self.get(path=path, repeat_delta=repeat_delta, repeat_if=reissue_processing)
         if call.recv.status != 200:
