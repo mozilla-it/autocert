79d7a9f66b780060ee1423b61ea21f88e1499a99
Scott Idler <scott.a.idler@gmail.com>
2018-08-22 11:19:42 -0700

    fixed BLACKLIST pathing after blacklist.py was moved out of utils/

--- api/blacklist.py
+++ api/blacklist.py
@@ -9,7 +9,7 @@ from utils.fmt import *
 
 try:
     thisdir = os.path.dirname(os.path.abspath(__file__))
-    items = open(fmt('{thisdir}/../BLACKLIST')).read().strip().split()
+    items = open(fmt('{thisdir}/BLACKLIST')).read().strip().split()
     BLACKLIST = [item for item in items if not item.startswith('#')]
 except Exception as ex:
     print('error happened when loading the BLACKLIST')
