ffbd4866550ea9ecde89012200b5a313c193152d
Scott Idler <scott.a.idler@gmail.com>
2018-09-07 16:24:26 -0700

    debug statement for json returned from get domains call

--- autocert/api/authority/digicert.py
+++ autocert/api/authority/digicert.py
@@ -189,6 +189,7 @@ class DigicertAuthority(AuthorityBase):
         call = self.get(fmt('domain?container_id={container_id}'))
         if call.recv.status != 200:
             raise DigicertError(call)
+        dbg(json=dict(call.recv.json))
         return [domain for domain in call.recv.json.domains if domain.is_active and domain.organization.id == organization_id]
 
     def _validate_domains(self, organization_id, container_id, domains, whois_check=False):
